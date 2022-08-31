9f4eaf7b3f81a2fa099bc57cfb1f9b7d28bdc866
joonhap <j.park@ku.edu>
2022-08-30 22:02:10 -0500

    starts writing SCL distribution function

---
+++ R/scl.R
@@ -0,0 +1,9 @@
+#' The SCL Distribution
+#'
+#' Quantile function, distribution function, and random generation for the SCL distribution.
+#'
+#' @param
+#' @return
+#' @export
+qscl <- function() {
+}
